1f85902fe5896d8e05e15f6b7acb083fb65c7ec9
barunslick <barunpradhan987@gmail.com>
2020-08-06 09:45:23 +0545

    Updates

---
+++ precache-manifest.9541f465891f52a596af014802c629c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc23e0bf44fdbf2999401ee763b1b4bb",
+    "url": "/github-ui/index.html"
+  },
+  {
+    "revision": "07100b1348c5a7a3e268",
+    "url": "/github-ui/static/css/main.429d2204.chunk.css"
+  },
+  {
+    "revision": "3727e5aeea9546b7e9c3",
+    "url": "/github-ui/static/js/2.d9a501db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-ui/static/js/2.d9a501db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07100b1348c5a7a3e268",
+    "url": "/github-ui/static/js/main.df3829c9.chunk.js"
+  },
+  {
+    "revision": "618e418fb182bd84ce80",
+    "url": "/github-ui/static/js/runtime-main.ab0ddf9e.js"
+  }
+]);
